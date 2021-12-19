67d1a5c4df680d0a750dd87e08f23aa844e0c620
LordRonz <aarontanhar2000@gmail.com>
2021-12-19 13:22:41 +0700

    style: formatting with black

--- draw.py
+++ draw.py
@@ -1,19 +1,24 @@
-from PIL import Image, ImageDraw
 from numpy import isscalar
+from PIL import Image, ImageDraw
+
 
 def draw(art, im, out, color):
-    img = Image.new('RGB', (1, 1))
+    img = Image.new("RGB", (1, 1))
     d = ImageDraw.Draw(img)
     char_width, char_height = d.textsize(art[0][0][0])
     char_height = char_width * 2
-    img = Image.new('RGB', (char_width * len(art[0][0]) * len(art[0]), char_height * len(art)))
+    img = Image.new(
+        "RGB", (char_width * len(art[0][0]) * len(art[0]), char_height * len(art))
+    )
     d = ImageDraw.Draw(img)
 
     for i, strings in enumerate(art):
         for j, string in enumerate(strings):
             r, g, b = 255, 255, 255
             if color:
-                r, g, b = (im[i, j], im[i, j], im[i, j]) if isscalar(im[i, j]) else im[i, j]
+                r, g, b = (
+                    (im[i, j], im[i, j], im[i, j]) if isscalar(im[i, j]) else im[i, j]
+                )
             d.text((j * char_width * 2, i * char_height), string, fill=(r, g, b))
 
     img.save(out)
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from ASCII_art_gen import generate_art
 from parser import get_args
+
+from ASCII_art_gen import generate_art
+
 
 def main():
     args = get_args()
-    input_filename = args['source_file'].name
-    args['source_file'].close()
-    output_filename = args['output'] or 'out.png'
-    generate_art(input_filename, output_filename, args['color'])
+    input_filename = args["source_file"].name
+    args["source_file"].close()
+    output_filename = args["output"] or "out.png"
+    generate_art(input_filename, output_filename, args["color"])
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
